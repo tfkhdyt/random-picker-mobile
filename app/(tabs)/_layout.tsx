7659caa092548be0ff5cdfa7988b39963b94c756
Taufik Hidayat <tfkhdyt@proton.me>
2025-03-11 08:26:20 +0700

    add create group button (not working yet)

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,23 @@
+import AntDesign from '@expo/vector-icons/AntDesign';
 import { Tabs } from 'expo-router';
+import { useState } from 'react';
+import { View } from 'react-native';
 
 import { TabBarIcon } from '../../components/TabBarIcon';
+
+import { Button } from '~/components/ui/button';
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '~/components/ui/dialog';
+import { Input } from '~/components/ui/input';
+import { Label } from '~/components/ui/label';
+import { Text } from '~/components/ui/text';
 
 export default function TabLayout() {
   return (
@@ -19,11 +36,6 @@ export default function TabLayout() {
         options={{
           title: 'Home',
           tabBarIcon: ({ color }) => <TabBarIcon type="antd" name="home" color={color} />,
-          // headerRight: () => (
-          //   <Link href="/modal" asChild>
-          //     <HeaderButton />
-          //   </Link>
-          // ),
         }}
       />
       <Tabs.Screen
@@ -31,6 +43,7 @@ export default function TabLayout() {
         options={{
           title: 'Group',
           tabBarIcon: ({ color }) => <TabBarIcon type="antd" name="bars" color={color} />,
+          headerRight: () => <CreateNewGroup />,
         }}
       />
       <Tabs.Screen
@@ -43,3 +56,45 @@ export default function TabLayout() {
     </Tabs>
   );
 }
+
+const CreateNewGroup = () => {
+  const [value, setValue] = useState('');
+
+  const handleSubmit = () => {};
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <View className="mr-2 rounded p-2 active:bg-gray-200">
+          <AntDesign name="pluscircleo" size={20} color="black" />
+        </View>
+      </DialogTrigger>
+      <DialogContent className="w-5/6">
+        <DialogHeader>
+          <DialogTitle>Create new group</DialogTitle>
+        </DialogHeader>
+        <View>
+          <Label nativeID="name" className="mb-2">
+            Name
+          </Label>
+          <Input
+            nativeID="name"
+            placeholder="Example: Games, food, etc"
+            value={value}
+            onChangeText={setValue}
+            onSubmitEditing={handleSubmit}
+            returnKeyType="send" // Changes the return key label
+            className="placeholder:text-base"
+          />
+        </View>
+        <DialogFooter>
+          <DialogClose asChild onPress={handleSubmit}>
+            <Button>
+              <Text>Save</Text>
+            </Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
